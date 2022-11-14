f7727ac0b9336d49fec43b3a805afd70c8163dc9
Nikita Shpilevoy <nick@shpil.io>
2022-11-14 10:06:04 +0000

    Update q2.py

--- q2.py
+++ q2.py
@@ -2,5 +2,10 @@ def create_function(a: int) -> callable:
   ...
 
 
-f = create_function(3)
-assert f(5) == 8
+f1 = create_function(3)
+assert f1(5) == 8
+assert f1(5) == 8
+assert f1(10) == 13
+
+f2 = create_function(100)
+assert f2(13) == 113
